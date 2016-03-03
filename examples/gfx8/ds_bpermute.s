e1836cd88645becdeb4e13ff95ad0f111fd78113
Nikolay Haustov <nikolay.haustov@amd.com>
2016-03-03 19:33:43 +0300

    Modify gfx8/ds_bpermute example to avoid use of private buffer.

--- examples/gfx8/ds_bpermute.s
+++ examples/gfx8/ds_bpermute.s
@@ -8,7 +8,6 @@
 hello_world:
 
    .amd_kernel_code_t
-      enable_sgpr_private_segment_buffer = 1
       enable_sgpr_kernarg_segment_ptr = 1
       is_ptr64 = 1
       compute_pgm_rsrc1_vgprs = 8
@@ -19,7 +18,8 @@ hello_world:
       workitem_vgpr_count = 6
   .end_amd_kernel_code_t
 
-  s_load_dwordx4  s[0:3], s[4:5], 0x00
+  s_load_dwordx2  s[4:5], s[0:1], 0x10
+  s_load_dwordx4  s[0:3], s[0:1], 0x00
   v_lshlrev_b32  v0, 2, v0
   s_waitcnt     lgkmcnt(0)
   v_add_u32     v1, vcc, s2, v0
@@ -30,12 +30,11 @@ hello_world:
   v_addc_u32    v4, vcc, v4, 0, vcc
   flat_load_dword  v1, v[1:2]
   flat_load_dword  v2, v[3:4]
-  s_load_dwordx2  s[0:1], s[4:5], 0x10
   s_waitcnt     vmcnt(0) & lgkmcnt(0)
   v_lshlrev_b32  v1, 2, v1
   ds_bpermute_b32  v1, v1, v2
-  v_add_u32     v3, vcc, s0, v0
-  v_mov_b32     v2, s1
+  v_add_u32     v3, vcc, s4, v0
+  v_mov_b32     v2, s5
   v_addc_u32    v4, vcc, v2, 0, vcc
   s_waitcnt     lgkmcnt(0)
   flat_store_dword  v[3:4], v1
